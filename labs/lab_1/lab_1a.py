f62583554d1e4a357e9f658ff3b8c9670fd4193d
BeanQueso <68991885+BeanQueso@users.noreply.github.com>
2026-03-16 14:27:22 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Eshaan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, I'm Eshaan. I'm a junior from Cypress, Texas, but I go to boarding school in New Hampshire.")
 
 if __name__ == "__main__":
     main()
